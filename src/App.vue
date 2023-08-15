828b136ab99b231e5bd4733e4b98180e2381003e
ThilinaSatoru <satoru.thilina@gmail.com>
2023-08-16 01:34:21 +0530

    yy

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Protostar</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <RouterLink class="nav-link active" aria-current="page" to="/"
+                >Home</RouterLink
+              >
+            </li>
+            <li class="nav-item">
+              <RouterLink
+                class="nav-link active"
+                aria-current="page"
+                to="/about"
+                >About</RouterLink
+              >
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link active" aria-current="page" to="/"
+                >Home</RouterLink
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,65 @@
+<script>
+import axios from "axios";
+
+export default {
+  name: "jobs",
+  data() {
+    return {
+      jobs: [],
+    };
+  },
+  mounted() {
+    this.getJobs();
+  },
+  methods: {
+    getJobs() {
+      axios.get("http://localhost:9091/api/v1/jobs/").then((res) => {
+        this.jobs = res.data;
+        console.log(this.jobs);
+      });
+    },
+  },
+};
+</script>
+
+<template>
+  <main>
+    <h1 class="text-center">Jobs</h1>
+    <div class="container">
+      <button class="btn btn-primary">+ New Job</button>
+      <table class="table">
+        <thead class="table-dark">
+          <tr>
+            <th>id</th>
+            <th>Name</th>
+            <th>JobClass</th>
+            <th>CRON</th>
+            <th>Active</th>
+            <th>Created</th>
+            <th>Latest</th>
+            <th>Websites</th>
+            <th>Cities</th>
+            <th>####</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item, index) in this.jobs" :key="index">
+            <td>{{ item.id }}</td>
+            <td>{{ item.name }}</td>
+            <td>{{ item.jobClass }}</td>
+            <td>{{ item.cron }}</td>
+            <td>{{ item.active }}</td>
+            <td>{{ item.createdOn }}</td>
+            <td>{{ item.lastExecutedOn }}</td>
+            <td>{{ item.websites }}</td>
+            <td>{{ item.cities }}</td>
+            <td>
+              <button class="btn btn-warning">Edit</button>
+              <button class="btn btn-danger">Del</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </main>
+</template>
